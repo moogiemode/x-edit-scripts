97798c3cebb8481c457405078a6239b7792bf0ac
Edwin Ekpenyong <moogiemode@gmail.com>
2024-03-22 23:33:16 -0700

    update qeuest

--- Fallout 4 Edit Scripts/__SMMWorkshopMenuPatch.pas
+++ Fallout 4 Edit Scripts/__SMMWorkshopMenuPatch.pas
@@ -5,8 +5,8 @@ uses praFunctions, mteFunctions, praUtil, moogieUtils;
 var
 	patchFile: IwbFile;
 	patchFileCreated: boolean;
-	mainFileKeywords: TList;
-	patchFileKeywords: TList;
+	mainFileKeywords, patchFileKeywords: TList;
+	questRecord: IInterface;
 
 //============================================================================
 
@@ -21,25 +21,112 @@ function Initialize: integer;
 
 //============================================================================
 
-function GetNewKeyword(keyword: IInterface): IInterface;
+function GetNewKeyword(selectedKeyword: IInterface): IInterface;
 	var
 		idx: integer;
-		copiedKeyword: IInterface;
+		copiedKeyword, linkedKeyword, referencedMenu: IInterface;
 		newEditorId: string;
 	begin
-		if (mainFileKeywords.IndexOf(keyword) = -1) then begin
-			copiedKeyword := wbCopyElementToFile(keyword, patchFile, true, true);
-			SetElementEditValues(copiedKeyword, 'EDID', 'SMM_' + GetElementEditValues(keyword, 'EDID') + '_' + (GetFilteredFileName(copiedKeyword, true)));
+		linkedKeyword := LinksTo(selectedKeyword);
+		if (mainFileKeywords.IndexOf(linkedKeyword) = -1) then begin
+			copiedKeyword := wbCopyElementToFile(linkedKeyword, patchFile, true, true);
+			SetElementEditValues(copiedKeyword, 'EDID', 'SMM_' + GetElementEditValues(linkedKeyword, 'EDID') + '_' + (GetFilteredFileName(selectedKeyword, true)));
 			//TODO - CREATE QUEST HERE TO INSERT KEYWORD INTO MENU VIA SMM
-			mainFileKeywords.Add(keyword);
+			referencedMenu := GetReferencedFormID(linkedKeyword);
+			CreateSMMQuestForKeyword(copiedKeyword, referencedMenu);
+			mainFileKeywords.Add(linkedKeyword);
 			patchFileKeywords.Add(copiedKeyword);
-			AddMessage('Adding keyword to main file list');
+			// AddMessage('Adding keyword to main file list');
 		end;
-		idx := mainFileKeywords.IndexOf(keyword);
+		idx := mainFileKeywords.IndexOf(linkedKeyword);
 		Result := ObjectToElement(patchFileKeywords[idx]);
 	end;
 //ObjectToElement(mainFileKeywords[i])
 //if (lst.IndexOf(s) = -1) then lst.Add(s);
+
+//============================================================================
+
+	function GetReferencedFormID(e: IInterface): IInterface;
+		var
+			i: integer;
+		begin
+		for i := 0 to ReferencedByCount(e) - 1 do begin
+			if Signature(ReferencedByIndex(e, i)) = 'FLST' then begin
+				Result := ReferencedByIndex(e, i);
+				exit;
+			end;
+		end;
+		Result := -1;
+	end;
+
+//============================================================================
+
+function CreateSMMQuestForKeyword(keyword: IInterface; menu: IInterface): IInterface;
+	var
+		questRecord, questScript, scriptProperty, structElements, curStruct, curMember: IInterface;
+	begin
+		questRecord := CreateNewRecordInFile(patchFile, 'QUST');
+		SetElementEditValues(questRecord, 'EDID', 'SMM_' + GetElementEditValues(keyword, 'EDID') + '_Quest');
+		questScript := AddScript(questRecord, 'SettlementMenuManager:MenuInstaller');
+
+
+		scriptProperty := createRawScriptProp(questScript, 'Author');
+		SetElementEditValues(scriptProperty, 'Type', 'String');
+		SetElementEditValues(scriptProperty, 'String', 'moogie');
+
+
+		curStruct := appendStructToProperty(getOrCreateScriptPropArrayOfStruct(questScript, 'Menus'));
+
+		CreateAndSetStructMember(curStruct, 'ModMenu', 'Object', keyword);
+
+		CreateAndSetStructMember(curStruct, 'TargetMenu', 'Object', menu);
+
+
+		// setPropertyValue(scriptProperty, menu);
+
+		// structElements := ElementByPath(scriptProperty, 'Value\Array of Struct');
+		// curStruct := Add(structElements, 'Member', true);
+		AddMessage(Path(curStruct));
+
+
+
+
+
+				// AddMessage('Creating quest for keyword: ' + GetElementEditValues(questScript, 'Properties\0\propertyName'));
+		// scriptProperties := ElementByPath(questScript, 'Properties');
+		// currentScriptProperty := ElementAssign(scriptProperties, HighInteger, nil, false);
+		// SetElementEditValues(currentScriptProperty, 'propertyName', 'Author');
+		// SetElementEditValues(currentScriptProperty, 'Type', 'String');
+		// SetElementEditValues(currentScriptProperty, 'String', 'moogie');
+
+		// currentScriptProperty := ElementAssign(scriptProperties, HighInteger, nil, false);
+		// SetElementEditValues(currentScriptProperty, 'propertyName', 'Menus');
+		// SetElementEditValues(currentScriptProperty, 'Type', 'Array of Struct');
+		// scriptValues := ElementByPath(currentScriptProperty, 'Value\Array of Struct');
+		// SetToDefault(curMember);
+		// // SetElementNativeValues(curMember, 'memberName', 'ModMenu');
+		// // SetElementEditValues(currentStruct, 'Flags', 'Edited');
+
+
+
+		// if (menu <> -1) then begin
+		// end;
+
+
+		//create the second member and add it with the flst that comes from references
+
+		Result := questRecord;
+	end;
+
+
+  // ElementAssign(ElementByPath(recBOOK, 'VMAD'), LowInteger, ElementByPath(tplBook, 'VMAD'), False);
+  // props := ElementByPath(recBOOK, 'VMAD\Data\Scripts\Script\Properties');
+  // prop := ElementByIndex(props, 0);
+  // SetElementEditValues(prop, 'String', GetElementEditValues(recBOOK, 'FULL'));
+  // prop := ElementByIndex(props, 2);
+  // SetElementEditValues(prop, 'Value\Object Union\Object v2\FormID', Name(recSOUN));
+
+
 //============================================================================
 
 function Process(e: IInterface): integer;
@@ -75,7 +162,9 @@ function Process(e: IInterface): integer;
 				newCraftingRecipe := wbCopyElementToFile(e, patchFile, false, true);
 				// newCraftingRecipe := getOverrideForElem(e, patchFile); //TEST this should perform the 2 above function calls of adding master and copying the record
 
-				SetElementNativeValues(newCraftingRecipe, 'FNAM\[0]', FormID(GetNewKeyword(keywordRecord)));
+				AddMessage(GetFilteredFileName(ElementByIndex(recKeywords, i), true));
+
+				SetElementNativeValues(newCraftingRecipe, 'FNAM\[' + IntToStr(i) + ']', FormID(GetNewKeyword(ElementByIndex(recKeywords, i))));
 
 
 
--- moogieUtils.pas
+++ moogieUtils.pas
@@ -1,5 +1,7 @@
 unit moogieUtils;
 
+uses praUtil;
+
 
   function GetFilteredFileName(e: IInterface; removeExt: boolean): string;
     var
@@ -7,10 +9,33 @@ unit moogieUtils;
     begin
       s := StringReplace(StringReplace(StringReplace(GetFileName(e), '[', '', [rfReplaceAll]), ']', ' -', [rfReplaceAll]), '_', ' ', [rfReplaceAll]);
 
-      if removeExt then
+      if removeExt then begin
         Result := Copy(s, 1, Length(s) - 4)
-      else
+      end else begin
         Result := s
+      end;
   end;
+
+  function CreateNewRecordInFile(e: IwbFile; signature: string): IInterface;
+    var
+      recGroup: IInterface;
+    begin
+      if not HasGroup(e, signature) then begin  // Add 'begin' here
+        recGroup := Add(e, signature, true); 
+      end else begin                            // Add 'begin' and 'end' for the else block
+        recGroup := GroupBySignature(e, signature);
+      end;  
+    Result := Add(recGroup, signature, true); // Does this belong inside the if-else?
+  end;
+
+  procedure CreateAndSetStructMember(struct: IInterface; memberName: string; memberType: string; value: variant);
+  var
+      curMember: IInterface;
+  begin
+    curMember := createRawStructMember(struct, memberName);
+		SetElementEditValues(curMember, 'Type', memberType);
+		setPropertyValue(curMember, value);
+  end;
+
 
 end.
